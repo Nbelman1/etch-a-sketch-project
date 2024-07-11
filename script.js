937c531b974099633adb3ccda1266cc21162395c
Nbelman1 <nbelman1@gmail.com>
2024-07-11 19:13:13 -0400

    Wrote createGrid function, added notes

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-let container = document.querySelector("#container");
+const boxContainer = document.querySelector(".box-container");
 
-function createDivs(num) {
-    for (i = 0; i < num; i++) {
-        let childDiv = document.createElement("div");
-        childDiv.classList.add("childDiv");
-        container.appendChild(childDiv);
+function createGrid(numSquares) {
+    for (i = 0; i < numSquares; i++) {
+        for (j = 0; j < numSquares; j++) {
+            const box = document.createElement("div");
+            box.classList.add("box");
+            boxContainer.appendChild(box);
+            box.style.width = `calc(100% / ${numSquares})`;
+            box.style.height = `calc(100% / ${numSquares})`;
+        }
     }
 }
 
-createDivs(16);
+createGrid(16);
