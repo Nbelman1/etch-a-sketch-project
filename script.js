965acf8ba11fd2face1bf75c5d3b3d62fcd1a12f
Nbelman1 <nbelman1@gmail.com>
2024-07-14 20:05:02 -0400

    Add event listener to squares. Add button to change grid size.

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
+// set querySelectors
 const boxContainer = document.querySelector(".box-container");
+const promptBtn = document.querySelector("#promptMe");
 
+// creates a grid with number of rows and columns of boxes defined by numSquares
+// box size scales to fit inside grid container
+// boxes change color to black when hovered over with mouse
 function createGrid(numSquares) {
     for (i = 0; i < numSquares; i++) {
         for (j = 0; j < numSquares; j++) {
-            const box = document.createElement("div");
-            box.classList.add("box");
-            boxContainer.appendChild(box);
-            box.style.width = `calc(100% / ${numSquares})`;
-            box.style.height = `calc(100% / ${numSquares})`;
+            const square = document.createElement("div");
+            square.classList.add("square");
+            boxContainer.appendChild(square);
+            square.style.width = `calc(100% / ${numSquares})`;
+            square.style.height = `calc(100% / ${numSquares})`;
+
+            square.addEventListener("mouseover", function(e) {
+                square.style.backgroundColor = "black";
+            });
         }
     }
 }
 
+// call createGrid function on page load
 createGrid(16);
+
+// prompt user to enter size of grid on button press
+// calls createGrid with user-specified number of squares 
+promptBtn.addEventListener("click", function(e) {
+    let userInput = prompt("How many squares should each row and column have?", "Enter number lower than 100");
+    if (userInput <= 100) {
+        const squares = boxContainer.querySelectorAll(".square");
+        squares.forEach((s) => {
+            s.remove();
+        });
+        createGrid(userInput);
+    } else {
+        alert("Your number is too high! Try again.");
+    }
+});
